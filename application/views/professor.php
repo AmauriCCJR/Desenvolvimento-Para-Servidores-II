56b6fb3363260450ab0496fb160ff0ee85ff9e5d
Amauri Carlos <amauricarlosdecarvalho@gmail.com>
2026-06-14 18:45:47 -0300

    pequenas correções

--- application/views/professor.php
+++ application/views/professor.php
@@ -8,30 +8,7 @@
 </head>
 
 <body>
-    <header>
-        <div id="headerMenu">
-            <a href="../Funcoes/indexPagina">
-                <h1 id="headerTitle">Mapeamento de Salas</h1>
-            </a>
-
-            <nav class="navbar navbar-expand-lg navbar-dark">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item"><a class="nav-link" href="../funcoes/abreSala">Sala de Aula</a></li>
-                        <li class="nav-item"><a class="nav-link" href="../funcoes/abreProfessor">Docente</a></li>
-                        <li class="nav-item"><a class="nav-link" href="../funcoes/abreTurma">Turma</a></li>
-                        <li class="nav-item"><a class="nav-link" href="../funcoes/abrePeriodo">Periodo</a></li>
-                        <li class="nav-item"><a class="nav-link" href="../funcoes/abreMapa">Reservas</a></li>
-                        <li class="nav-item"><a class="nav-link" href="../funcoes/abreRelatorio">Relatorio</a></li>
-                    </ul>
-                </div>
-            </nav>
-        </div>
-    </header>
+    <?php include '../Desenvolvimento-Para-Servidores-II/assets/includes/header.php' ?>
 
 
     <main>

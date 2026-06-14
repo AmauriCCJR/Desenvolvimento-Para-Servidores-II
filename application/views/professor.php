e505972df55d1a08b2da76ac19b55dd25443bc73
Amauri Carlos <amauricarlosdecarvalho@gmail.com>
2026-06-14 18:40:42 -0300

    Professor finalizado

--- application/views/professor.php
+++ application/views/professor.php
@@ -40,92 +40,7 @@
                 <input type="text" id="inputPesquisa" class="form-control" placeholder="Pesquisar" onkeyup="filtrarTabela()">
                 <button class="btn btn-outline-primary btnAcao modalBtn" id="botaoModal" type="button" data-bs-toggle="modal" data-bs-target="#cadastroProfessorModal">Cadastrar novo docente</button>
             </div>
-
-            <div class="modal fade" id="cadastroProfessorModal" tabindex="-1" role="dialog" aria-labelledby="cadastroProfessorModalLabel" aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="cadastroProfessorModalLabel">Cadastrar Novo Docente</h5>
-                            <button type="button" class="close" data-bs-dismiss="modal" aria-label="Fechar">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-
-                        <form id="formCadastroProfessor" method="post" class="modal-content">
-                            <div class="modal-body">
-                                <div class="form-group">
-                                    <label for="nome">Nome</label>
-                                    <input type="text" id="nome" name="nome" class="form-control" required>
-                                </div>
-                                <div class="form-group row">
-                                    <div class="col-sm-6">
-                                        <label for="cpf" class="col-form-label">CPF</label>
-                                        <input type="number" id="cpf" name="cpf" class="form-control" required>
-                                    </div>
-                                </div>
-
-                                <div class="col-sm-6">
-                                    <label for="tipo" class="col-form-label">Tipo</label>
-                                    <select name="tipo" id="tipo" class="form-control" required>
-                                        <option value="">Selecione</option>
-                                        <option value="F">Funcionário</option>
-                                        <option value="C">Carta Convite</option>
-                                    </select>
-                                </div>
-                            </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btnAcao" data-bs-dismiss="modal">Fechar</button>
-                            <button type="submit" class="btn btnAcao" onclick="cadastro();">Cadastrar</button>
-                        </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-
-            <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editModalLabel" aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="editModalLabel">Editar Docente</h5>
-                            <button type="button" class="close" data-bs-dismiss="modal" aria-label="Fechar">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <form id="formEditProfessor" method="post">
-                            <div class="modal-body">
-                                <input type="hidden" id="editId" name="editId">
-                                <div class="form-group">
-                                    <label for="editNome">Nome</label>
-                                    <input type="text" id="editNome" name="editNome" class="form-control" required>
-                                </div>
-                                <div class="form-group row">
-                                    <div class="col-sm-6">
-                                        <label for="editCpf" class="col-form-label">CPF</label>
-                                        <input type="number" id="editCpf" name="editCpf" class="form-control" required>
-                                    </div>
-                                    <div class="col-sm-6">
-                                        <label for="editTipo" class="col-form-label">Tipo</label>
-                                        <select name="editTipo" id="editTipo" class="form-control" required>
-                                            <option value="">Selecione</option>
-                                            <option value="F">Funcionario</option>
-                                            <option value="C">Carta Convite</option>
-                                        </select>
-                                    </div>
-
-                                </div>
-                                
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btnAcao" data-bs-dismiss="modal">Fechar</button>
-                                <button type="submit" class="btn btnAcao" onclick="editarProfessor();">Salvar</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
         </section>
-
-
 
         <section id="mostrarCadastro">
             <div class="table-responsive">
@@ -146,10 +61,90 @@
         </section>
     </main>
 
+    <!-- Modal Cadastro -->
+    <div class="modal fade" id="cadastroProfessorModal" tabindex="-1" role="dialog" aria-labelledby="cadastroProfessorModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="cadastroProfessorModalLabel">Cadastrar Novo Docente</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Fechar"></button>
+                </div>
+                <form id="formCadastroProfessor" method="post">
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label for="codigo">Código</label>
+                            <input type="number" id="codigo" name="codigo" class="form-control" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="nome">Nome</label>
+                            <input type="text" id="nome" name="nome" class="form-control" required>
+                        </div>
+                        <div class="form-group row">
+                            <div class="col-sm-6">
+                                <label for="cpf" class="col-form-label">CPF</label>
+                                <input type="number" id="cpf" name="cpf" class="form-control" required>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <label for="tipo" class="col-form-label">Tipo</label>
+                            <select name="tipo" id="tipo" class="form-control" required>
+                                <option value="">Selecione</option>
+                                <option value="F">Funcionário</option>
+                                <option value="C">Carta Convite</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btnAcao" data-bs-dismiss="modal">Fechar</button>
+                        <button type="submit" class="btn btnAcao" onclick="cadastro();">Cadastrar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal Editar -->
+    <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="editModalLabel">Editar Docente</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Fechar"></button>
+                </div>
+                <form id="formEditProfessor" method="post">
+                    <div class="modal-body">
+                        <input type="hidden" id="editId" name="editId">
+                        <div class="form-group">
+                            <label for="editNome">Nome</label>
+                            <input type="text" id="editNome" name="editNome" class="form-control" required>
+                        </div>
+                        <div class="form-group row">
+                            <div class="col-sm-6">
+                                <label for="editCpf" class="col-form-label">CPF</label>
+                                <input type="number" id="editCpf" name="editCpf" class="form-control" required>
+                            </div>
+                            <div class="col-sm-6">
+                                <label for="editTipo" class="col-form-label">Tipo</label>
+                                <select name="editTipo" id="editTipo" class="form-control" required>
+                                    <option value="">Selecione</option>
+                                    <option value="F">Funcionario</option>
+                                    <option value="C">Carta Convite</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btnAcao" data-bs-dismiss="modal">Fechar</button>
+                        <button type="submit" class="btn btnAcao" onclick="editarProfessor();">Salvar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 
 
+
+    <script src="../assets/js/professor.js"></script>
 </body>
 
 </html>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="../assets/js/professor.js" defer></script>
--- assets/js/professor.js
+++ assets/js/professor.js
@@ -2,21 +2,227 @@ async function cadastro() {
     event.preventDefault();
 
     try{
+        const codigo = document.getElementById('codigo').value;
         const nome = document.getElementById('nome').value;
         const cpf = document.getElementById('cpf').value;
         const tipo = document.getElementById('tipo').value;
 
         const response = await fetch('../Professor/inserir', {method:'POST', headers: {'Content-Type': 'application/json'}, 
-            body: JSON.stringify({nome: nome, cpf: cpf, tipo: tipo})});
+            body: JSON.stringify({codigo: codigo, nome: nome, cpf: cpf, tipo: tipo})});
         const result = await response.json();
 
             if (result.codigo == 1){
-                $('#cadastroProfessorModal').modal('hide');
+                bootstrap.Modal.getInstance(document.getElementById('cadastroProfessorModal')).hide();
                 Swal.fire('Sucesso!', result.msg, 'success');
                 carregarDados();
+            } else {
+                const mensagemDeErro = result.erros.map(erro => {
+                    return `<p><strong>[${erro.campo ?? erro.codigo}]</strong> ${erro.msg}</p>`;
+                }).join('');
+
+                Swal.fire({
+                    title: 'Houve(ram) erro(s) de validação: ',
+                    html: mensagemDeErro,
+                    icon: 'error',
+                    confirmButtonText: 'Fechar'
+                });
             }
 
     } catch (error){
+        console.error('Erro ao cadastrar o professor: ', error);
+        Swal.fire('Erro', 'Ocorreu um erro ao processar a requisição. ', 'error');
+    }
+}
 
+async function carregarDados() {
+    try {
+        const response = await fetch('../Professor/consultar', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                codigo: '',
+                nome: '',
+                cpf: '',
+                tipo: ''
+            })
+        });
+        const data = await response.json();
+        const conteudoAcesso = document.getElementById('conteudo-professor');
+
+        conteudoAcesso.innerHTML = '';
+
+        if (!data.sucesso || !data.dados || data.dados.length === 0) {
+            conteudoAcesso.innerHTML = '<tr><td colspan="4">Nenhum registro encontrado.</td></tr>';
+            return;
+        }
+
+        data.dados.forEach(item => {
+            let tipo = item.tipo;
+            if (tipo == 'F'){
+                tipo = 'Funcionário'
+            } else {
+                tipo = 'Carta Convite'
+            }
+
+            const codigo = item.codigo;
+            conteudoAcesso.innerHTML += `
+            <tr class="alert alert-warning">
+                <td>${item.nome}</td>
+                <td>${item.cpf}</td>
+                <td>${tipo}</td>
+                <td>
+                    <div class="row">
+                    <button class="btn btn-warning btnAcao" onclick="openEditModal(${item.codigo}, this)">
+                        <i class="fas fa-pencil"></i>
+                    </button>
+                    <button class="btn btn-danger btnAcao btnAcaoExcluir" onclick="deletarProfessor(${item.codigo})">
+                        <i class="fas fa-trash"></i>
+                    </button>
+                    </div>
+                </td>
+            </tr>
+            `;
+        });
+
+    } catch (error){
+        console.error('Erro ao carregar os dados: ', error);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    carregarDados();
+
+    const cadastroModal = document.getElementById('cadastroProfessorModal');
+    cadastroModal.addEventListener('show.bs.modal', function(){
+        document.getElementById('formCadastroProfessor').reset();
+    });
+});
+
+function openEditModal(codigo, button){
+    const row = button.closest('tr');
+
+    const nome = row.cells[0].innerText;
+    const cpf = row.cells[1].innerText;
+    const tipo = row.cells[2].innerText.charAt(0);
+
+    document.getElementById('editId').value = codigo;
+    document.getElementById('editNome').value = nome;
+    document.getElementById('editCpf').value = cpf;
+    document.getElementById('editTipo').value = tipo;
+
+    new bootstrap.Modal(document.getElementById('editModal')).show();
+}
+
+async function editarProfessor(){
+    event.preventDefault();
+
+    try{
+        const codigo = document.getElementById('editId').value;
+        const nome = document.getElementById('editNome').value;
+        const cpf = document.getElementById('editCpf').value;
+        const tipo = document.getElementById('editTipo').value;
+
+        const response = await fetch('../Professor/alterar', {method:'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify({
+            codigo: codigo,
+            nome: nome,
+            cpf: cpf,
+            tipo: tipo
+        })});
+
+        const result = await response.json();
+
+        if (result.codigo == 1){
+            bootstrap.Modal.getInstance(document.getElementById('editModal')).hide();
+            Swal.fire('Sucesso!', result.msg, 'success');
+            carregarDados();
+        } else {
+            const mensagemDeErro = result.erros.map(erro => {
+                return `<p><strong>[${erro.campo ?? erro.codigo}]</strong> ${erro.msg}</p>`;
+            }).join('');
+
+            Swal.fire({
+                title: 'Houve(ram) erros(s) de validação: ',
+                html: mensagemDeErro,
+                icon: 'error',
+                confirmButtonText: 'Fechar'
+            });
+        }
+
+    } catch(error){
+        console.error('Erro ao editar o professor: ', error);
+        Swal.fire('Erro', 'Ocorreu um erro ao processar a requisição.', 'error');
+    }
+}
+
+
+async function deletarProfessor(codigo){
+    Swal.fire({
+        title: 'Atenção!',
+        text: 'Tem certeza que deseja remover esse professor?',
+        icon: 'question',
+        showConfirmButton: true,
+        showCancelButton: true,
+        customClass: {
+            popup: 'my-swal-popup',
+            title: 'my-swal-title',
+            html: 'my-swal-text',
+            confirmButton: 'btn btn-danger btnAcao my-swal-button',
+            cancelButton: 'btn btn-secondary btnAcao my-swal-button'
+        },
+        buttonsStyling: false,
+    }).then(async function(res){
+        if (res.isConfirmed){
+            const config = {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    codigo: codigo
+                })
+            };
+            const request = await fetch('../Professor/desativar', config);
+            const response = await request.json();
+
+            Swal.fire({
+                title: 'Atenção!',
+                text: response.msg,
+                icon: response.codigo == 1? 'success' : 'error',
+                customClass: {
+                    popup: 'my-swal-popup',
+                    title: 'my-swal-title',
+                    html: 'my-swal-text',
+                    confirmButton: 'btn btn-primary btnAcao'
+                },
+                buttonsStyling: false
+            });
+            carregarDados();
+        }
+    });
+}
+
+
+function filtrarTabela(){
+    const input = document.getElementById('inputPesquisa');
+    const filter = input.value.toLowerCase();
+    const tabela = document.getElementById('conteudo-professor');
+    const linhas = tabela.getElementsByTagName('tr');
+
+    for (let i=0; i< linhas.length; i++){
+        const colProfessor = linhas[i].getElementsByTagName("td")[0]; 
+        const colCpf = linhas[i].getElementsByTagName('td')[1];
+        const colTipo = linhas[i].getElementsByTagName('td')[2];
+
+        if (colProfessor){
+            const professorTexto = colProfessor.textContent || colProfessor.innerText;
+            const tipoTexto = colTipo.textContent || colTipo.innerText;
+            const cpfTexto = colCpf.textContent || colCpf.innerText;
+
+            if (professorTexto.toLowerCase().indexOf(filter) > -1 || tipoTexto.toLocaleLowerCase().indexOf(filter) > -1){
+                linhas[i].style.display = "";
+            } else {
+                linhas[i].style.display = "none";
+            }
+        }
     }
 }
